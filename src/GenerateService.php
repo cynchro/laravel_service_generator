ddca6548c2a3002fb3abf8952a601e9b630f4925
cynchro <alexissaucedo@gmail.com>
2023-06-08 15:53:17 -0300

    new kernel add dependencies (#1)
    Co-authored-by: alexisCobax <alexiscobax1@gmail.com>

--- src/GenerateService.php
+++ src/GenerateService.php
@@ -38,12 +38,45 @@ class GenerateService extends Command
     {
         $stubPath = base_path('vendor/bacoder/servicesgenerator/stubs/services/service.stub');
         $stubContent = File::get($stubPath);
-        $content = str_replace(
-            ['{{ModelName}}', '{{ClassName}}', '{{modelName}}'],
-            [$modelName, $className, lcfirst($modelName)],
-            $stubContent
-        );
+        $replacements = [
+            '{{ModelName}}' => $modelName,
+            '{{ClassName}}' => $className,
+            '{{modelName}}' => lcfirst($modelName),
+        ];
+        $content = str_replace(array_keys($replacements), array_values($replacements), $stubContent);
 
         return $content;
     }
+
+    public static function registerCommand()
+    {
+        $kernelFile = base_path('app/Console/Kernel.php');
+        $content = file_get_contents($kernelFile);
+
+        $commandClass = 'GenerateService::class';
+        $useStatement = 'use Bacoder\Servicesgenerator\GenerateService;';
+        
+        if (strpos($content, $commandClass) === false) {
+            $content = str_replace(
+                'protected $commands = [',
+                "protected \$commands = [\n        {$commandClass},",
+                $content
+            );
+        }
+
+        if (strpos($content, $useStatement) === false) {
+            $useStatement = "use Bacoder\Servicesgenerator\GenerateService;\n";
+            $content = preg_replace(
+                '/(use [^;]+;)/',
+                "$1\n" . $useStatement,
+                $content,
+                1
+            );
+        }
+
+        file_put_contents($kernelFile, $content);
+    }
 }
+
+GenerateService::registerCommand();
+
