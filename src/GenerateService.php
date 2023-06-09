1965c0c113d9b77d8678063520931c998a7c4a9e
alexisCobax <alexiscobax1@gmail.com>
2023-06-09 13:35:43 -0300

    [feature] add controller created like service

--- src/GenerateService.php
+++ src/GenerateService.php
@@ -8,20 +8,25 @@ use Illuminate\Support\Facades\File;
 class GenerateService extends Command
 {
     protected $signature = 'generate:service {model}';
-    protected $description = 'Generate a service class based on a model';
+    protected $description = 'Generate a service class and controller based on a model';
 
     public function handle()
     {
         $model = $this->argument('model');
         $modelName = $this->generateModelName($model);
         $className = $this->generateClassName($model);
-        $content = $this->generateContent($modelName, $className);
-        $filePath = app_path('Services/' . $className . 'Service.php');
+        $serviceContent = $this->generateServiceContent($modelName, $className);
+        $controllerContent = $this->generateControllerContent($modelName, $className);
+        $serviceFilePath = app_path('Services/' . $className . 'Service.php');
+        $controllerFilePath = app_path('Http/Controllers/' . $className . 'Controller.php');
 
         File::ensureDirectoryExists(app_path('Services'));
-        File::put($filePath, $content);
+        File::put($serviceFilePath, $serviceContent);
 
-        $this->info("The service class '{$className}Service' has been generated successfully.");
+        File::ensureDirectoryExists(app_path('Http/Controllers'));
+        File::put($controllerFilePath, $controllerContent);
+
+        $this->info("The service class '{$className}Service' and the controller class '{$className}Controller' have been generated successfully.");
     }
 
     private function generateModelName($model)
@@ -34,7 +39,7 @@ class GenerateService extends Command
         return ucfirst($model);
     }
 
-    private function generateContent($modelName, $className)
+    private function generateServiceContent($modelName, $className)
     {
         $stubPath = base_path('vendor/bacoder/servicesgenerator/stubs/services/service.stub');
         $stubContent = File::get($stubPath);
@@ -48,35 +53,17 @@ class GenerateService extends Command
         return $content;
     }
 
-    public static function registerCommand()
+    private function generateControllerContent($modelName, $className)
     {
-        $kernelFile = base_path('app/Console/Kernel.php');
-        $content = file_get_contents($kernelFile);
+        $stubPath = base_path('vendor/bacoder/servicesgenerator/stubs/controllers/controller.stub');
+        $stubContent = File::get($stubPath);
+        $replacements = [
+            '{{ModelName}}' => $modelName,
+            '{{ClassName}}' => $className,
+            '{{modelName}}' => lcfirst($modelName),
+        ];
+        $content = str_replace(array_keys($replacements), array_values($replacements), $stubContent);
 
-        $commandClass = 'GenerateService::class';
-        $useStatement = 'use Bacoder\Servicesgenerator\GenerateService;';
-        
-        if (strpos($content, $commandClass) === false) {
-            $content = str_replace(
-                'protected $commands = [',
-                "protected \$commands = [\n        {$commandClass},",
-                $content
-            );
-        }
-
-        if (strpos($content, $useStatement) === false) {
-            $useStatement = "use Bacoder\Servicesgenerator\GenerateService;\n";
-            $content = preg_replace(
-                '/(use [^;]+;)/',
-                "$1\n" . $useStatement,
-                $content,
-                1
-            );
-        }
-
-        file_put_contents($kernelFile, $content);
+        return $content;
     }
 }
-
-GenerateService::registerCommand();
-
